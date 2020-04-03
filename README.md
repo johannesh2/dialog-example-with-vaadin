8f52d750bf225bc4ae1b4352010c2373f9137721
Johannes Häyry <johannes.hayry@vaadin.com>
2020-04-03 11:38:46 +0300

    Cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+# An example of using new dialog features in Vaadin 14.2
+
+
--- src/main/java/org/vaadin/johannes/MainView.java
+++ src/main/java/org/vaadin/johannes/MainView.java
@@ -10,6 +10,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.johannes.components.dialogs.MyDialog;
 
 /**
  * A sample Vaadin view class.
@@ -24,10 +25,6 @@ import org.springframework.beans.factory.annotation.Autowired;
  * browser tab/window.
  */
 @Route
-@PWA(name = "Vaadin Application",
-        shortName = "Vaadin App",
-        description = "This is an example Vaadin application.",
-        enableInstallPrompt = true)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 @CssImport(value = "./styles/dialog-overlay.css", themeFor = "vaadin-dialog-overlay")
@@ -37,30 +34,18 @@ public class MainView extends VerticalLayout {
      * Construct a new Vaadin view.
      * <p>
      * Build the initial UI state for the user accessing the application.
-     *
-     * @param service The message service. Automatically injected Spring managed bean.
      */
-    public MainView(@Autowired GreetService service) {
+    public MainView() {
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+        MyDialog myDialog = new MyDialog();
 
         // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
+        Button button = new Button("Open demo dialog",
+                event -> myDialog.open());
         button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
         button.addClickShortcut(Key.ENTER);
 
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        add(button);
     }
 
 }
