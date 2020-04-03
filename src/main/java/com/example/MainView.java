70277b87eb55a196e30084cab7fbe2ed50886113
Johannes Häyry <johannes.hayry@vaadin.com>
2020-04-03 14:27:57 +0300

    Renamed packages

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -15,20 +15,21 @@ import org.vaadin.johannes.components.dialogs.MyDialog;
 /**
  * A sample Vaadin view class.
  * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
+ * To implement a Vaadin view just extend any Vaadin component and use @Route
+ * annotation to announce it in a URL as a Spring managed bean. Use the @PWA
+ * annotation make the application installable on phones, tablets and some
+ * desktop browsers.
  * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
+ * A new instance of this class is created for every new user and every browser
+ * tab/window.
  */
 @Route
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 @CssImport(value = "./styles/dialog-overlay.css", themeFor = "vaadin-dialog-overlay")
 public class MainView extends VerticalLayout {
+
+    MyDialog myDialog;
 
     /**
      * Construct a new Vaadin view.
@@ -37,15 +38,19 @@ public class MainView extends VerticalLayout {
      */
     public MainView() {
 
-        MyDialog myDialog = new MyDialog();
-
         // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Open demo dialog",
-                event -> myDialog.open());
+        Button button = new Button("Open demo dialog", event -> openTheDialog());
         button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        button.addClickShortcut(Key.ENTER);
 
         add(button);
+    }
+
+    private void openTheDialog() {
+        if (myDialog != null) {
+            myDialog.close();
+        }
+        myDialog = new MyDialog();
+        myDialog.open();
     }
 
 }
